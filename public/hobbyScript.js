0edd58ae0466dd3847f15d5519af9d871a7fffda
Diemoonn <gordienko.gordienkodima2004@yandex.ru>
2023-09-21 11:54:21 +0300

    Hobby page with the first version of text has been added

---
+++ public/hobbyScript.js
@@ -0,0 +1,26 @@
+function back() {
+    location.href = 'index.html';
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    const contentContainer = document.getElementById('parsedText');
+
+    const arguments = {
+        DOCXPath: 'public/docs/hobby/textbox.docx'
+    }
+  
+    fetch('/parseDocx', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(arguments)
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        contentContainer.innerHTML = data.content; // Use innerHTML to preserve HTML formatting
+      })
+      .catch((error) => {
+        console.error('Error fetching content:', error);
+      });
+  });
--- server.js
+++ server.js
@@ -1,15 +1,36 @@
+/* import required server libs */
+
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
+/* Serve static files from the public directory */
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 /* Logic */
 
+// DOCX parser
+app.use(express.json());
 
+const mammoth = require('mammoth');
 
-/* Server stuff */
+app.post('/parseDocx', (req, res) => {
+  const docxPath = req.body.DOCXPath;
+
+  mammoth.convertToHtml({ path: docxPath })
+    .then((result) => {
+      const content = result.value;
+      res.json({ content });
+    })
+    .catch((error) => {
+      console.error('Error converting file:', error);
+      res.status(500).json({ error: 'Error converting file' });
+    });
+});
+
+/* Server initialization */
 
 const port = 3000; // Change this to the desired port number.
 app.listen(port, () => {
